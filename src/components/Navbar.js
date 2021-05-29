ddb2754cd5af86d28d236c5399816291c616aaee
unknown <srparadkar15@gmail.com>
2021-05-29 20:44:25 +0530

    Added authentication via discord

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Navbar = () => {
+    useEffect(() => {
+        const fragment = new URLSearchParams(window.location.hash.slice(1));
+        const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+        if (!accessToken) {
+            console.log('access token not found');
+        }
+        else {
+            axios.get('https://discord.com/api/users/@me', {
+                headers: {
+                    authorization: `${tokenType} ${accessToken}`,
+                },
+            })
+            .then(result => {
+                console.log('result', result.data);
+                console.log('img', 'https://cdn.discordapp.com/avatars/' + result.data.id +'/'+ result.data.avatar);
+            })
+            .catch(console.error);
+        }
+    }, []);
     return (
         <nav className='flex justify-between items-center h-16 bg-white text-black
         relative shadow-sm font-mono' role='navigation'>
            <Link to='/' className="pl-8">
                Hack Club
             </Link>
-            <div className="pbnx-4 cursor-pointer md:hidden">
+            {/* <div className="pbnx-4 cursor-pointer md:hidden">
                 <svg 
                     className="w-6 h-6" 
                     fill="currentColor" 
@@ -26,7 +46,9 @@ const Navbar = () => {
                 <Link className="p-4" to='/'>Home</Link>
                 <Link className="p-4" to='/'>About</Link>
                 <Link className="p-4" to='/'>Leaderboard</Link>
-            </div>
+            </div> */}
+            <button><a
+                href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FSpilled-My-Coffee-Here-UI&response_type=token&scope=identify`}>Auth</a></button>
         </nav>
     )
 }
